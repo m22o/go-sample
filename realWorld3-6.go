4cef2a29c3e80f39a8ddf5d0b7d13bd04b543651
takurou Matsuo <m.taku7@gmail.com>
2017-08-06 17:38:16 +0900

    3.8

---
+++ realWorld3-6.go
@@ -0,0 +1,18 @@
+// curl --head http://localhost:18888
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// http.GETの代わりにhttp.Headを使うだけ
+	resp, err := http.Head("http://localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+	log.Println("Headers:", resp.Header)
+}
